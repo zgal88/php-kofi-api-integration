6869aa67d72d0e13e201c8aa1d03713ee5b2d275
Anna <17416592+zgal88@users.noreply.github.com>
2022-03-08 02:43:34 -0500

    Bug Fixes
    Renamed discord file, for easier file names. Fixed bug that was preventing sending webhook message.

--- index.php
+++ index.php
@@ -8,53 +8,74 @@
 //  September 12th, 2021                                                                               //
 //  Listed under the MIT License                                                                       //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Set to 'true' if you want to enable the Discord webhook sender.
-$send_discord_webhook = false;
-// Discord Webhook URL - Required if sending
-// Change this to your own webhook address
-$hook_url = "https://discord.com/api/webhooks/123456789123456789/aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567812345678";
-// First check if a 'data' object was even sent
-if(isset($_POST['data'])){
-	try{
-		// Grabs The payload, and decodes it.
-		$data = $_POST['data'];
-		$array = json_decode($data, true);
-		// Assign variables from data array values.
-		$messageID = $array['message_id'];
-		$kofiTransactionID = $array['kofi_transaction_id'];
-		$donationTimeStamp = $array['timestamp'];
-		// Possible Types Include Donation, Subscription, Commision, and Shop Order.
-		$donationType = $array['type'];
-		$donationFrom = $array['from_name'];
-		$donationMessage = $array['message'];
-		$donationAmount = $array['amount'];
-		$donationCurrency = $array['currency'];
-		$donationURL = $array['url'];
-		$donationIsSubscription = $array['is_subscription_payment'];
-		$donationIsFirstSubPayment = $array['is_first_subscription_payment'];
-		////////////////////////////////////////////////////////////////////////////
-		//                             Do Stuff Here                              //
-		////////////////////////////////////////////////////////////////////////////
-		// Send the 200 Ok Header and end function
-		header($_SERVER["SERVER_PROTOCOL"]." 200 Ok");
-		die;
-	}catch(Exception $exc){
-		// Some shizz happened, so let's log it for you
-		$message = "\r\nException: " . $exc;
-		// Check if there was a JSON decoding problem, and add to log if so.
-		if(json_last_error()){ $message .= "\r\nJSON Decode Error Message: " . json_last_error_msg();
-		// Open logs fille, append, and close.
-		$errorLog = fopen('error_log.txt', 'a');
-		fwrite($errorLog, $message);
-		fclose($errorLog);
-		// Now let's dump what was collected from the data object, for troubleshooting, just in case.
-		$objectDumpFile = fopen(time() . '-data_dump.txt', 'a');
-		fwrite($objectDumpFile, $data);
-		fclose($objectDumpFile);
-		// Send error code header so Ko-Fi knows the data wasn't recieved/processed properly.
-		header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error");
-		die;
+try{
+	// send_discord_webhook : set to 'true' if you want to enable the Discord webhook sender.
+	// Assumes send-discord.php is in the same folder as this file.
+	//
+	// hook_url : Required if sending - change this to your own webhook address. Will fail
+	// if you do not change it.
+	$send_discord_webhook = true;
+	$hook_url = "https://discord.com/api/webhooks/123456789123456789/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567887654321";
+	// First check if a 'data' object was even sent
+	if(isset($_POST['data'])){
+		try{
+			// Grabs The payload, and decodes it.
+			$data = $_POST['data'];
+			$array = json_decode($data, true);
+			// Assign variables from data array values.
+			$messageID = (($array['message_id'] != "") ? $array['message_id'] : "None");
+			$kofiTransactionID = (($array['kofi_transaction_id'] != "") ? $array['kofi_transaction_id'] : "None");
+			// When the donation was sent
+			$donationTimeStamp = (($array['timestamp'] != "") ? $array['timestamp'] : "None");
+			// The donation type - Possible Types Include Donation, Subscription, Commission, and Shop Order.
+			$donationType = (($array['type'] != "") ? $array['type'] : "None");
+			// Display name for who ssent the donation; not necessarily their Ko-Fi username.
+			$donationFrom = (($array['from_name'] != "") ? $array['from_name'] : "None");
+			// If they left a message, it's here.
+			$donationMessage = (($array['message'] != "") ? $array['message'] : "None");
+			// The numerical value of the donation amount
+			$donationAmount = (($array['amount'] != "") ? $array['amount'] : "None");
+			// The donation currency
+			$donationCurrency = (($array['currency'] != "") ? $array['currency'] : "None");
+			// URL of donaiton
+			$donationURL = (($array['url'] != "") ? $array['url'] : "None");
+			// Is the donation a subscription?
+			$donationIsSubscription = (($array['is_subscription_payment'] != "") ? $array['is_subscription_payment'] : "None");
+			// Is iot the first payment in the subscription?
+			$donationIsFirstSubPayment = (($array['is_first_subscription_payment'] != "") ? $array['is_first_subscription_payment'] : "None");
+			
+			////////////////////////////////////////////////////////////////////////////
+			//                                                                        //
+			//                  Do Stuff Here With The Gathered Data                  //
+			//                                                                        //
+			////////////////////////////////////////////////////////////////////////////
+			
+			// Send Discord Webhook, If Enabled
+			if($send_discord_webhook == true) require('send-discord.php');
+			// Send the 200 Ok Header and end function
+			header($_SERVER["SERVER_PROTOCOL"]." 200 Ok");
+		}catch(Exception $exc){
+			// Some shizz happened, so let's log it for you
+			$message = "\r\nException: " . $exc->getMessage();
+			// Check if there was a JSON decoding problem, and add to log if so.
+			if(json_last_error()) $message .= "\r\nJSON Decode Error Message: " . json_last_error_msg();
+			// Open logs fille, append, and close.
+			$errorLog = fopen('error_log.txt', 'a');
+			fwrite($errorLog, $message);
+			fclose($errorLog);
+			// Now let's dump what was collected from the data object, for troubleshooting, just in case.
+			$objectDumpFile = fopen(time() . '-data_dump.txt', 'a');
+			fwrite($objectDumpFile, $data);
+			fclose($objectDumpFile);
+			// Send error code header so Ko-Fi knows the data wasn't recieved/processed properly.
+			header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error");
+		}
 	}
+	// Gracefully close out everything, and tell the function to exit.
+	die;
+}catch(Exception $exc){
+	// Just on the offchance something got messed up prior to collecting data
+	// Logs it here to error.txt log
+	file_put_contents('error.txt', $exc->getMessage());
 }
-if($send_discord_webhook) require('send-discord-webhook-notification.php');
 ?>
---
+++ send-discord.php
@@ -0,0 +1,40 @@
+<?php
+///////////////////////////////////////////////
+//  This is a companion file that assumes    //
+//  you have first gathered the data using   //
+//  the main index.php file in this project  //
+///////////////////////////////////////////////
+// Make Sure We Have A Valid Message
+if(isset($donationMessage)){
+	$donationMessage = (($donationMessage != "" && $donationMessage != null) ? $donationMessage : "No Donation Message Provided");
+}
+if(!isset($donationMessage)){
+	$donationMessage = "No Donation Message Provided";
+}
+// Construct Data Object To Be Sent
+try{
+	$data = [
+        'embeds' => [[
+            'title' => $donationType . "Received!",
+            'description' => "Message from Sender: " . $donationMessage,
+            'timestamp' => $donationTimeStamp,
+            'color' => hexdec("13C3FF"),
+            'author' => [
+                'name' => $donationFrom,
+                'url' => 'https://ko-fi.com',
+                'icon_url' => "https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/61e1116779fc0a9bd5bdbcc7_Frame%206.png",
+            ],
+        ]],
+	];
+	$curl = curl_init($hook_url);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($curl, CURLOPT_HEADER, 0);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	$discord_response = curl_exec($curl);
+	curl_close($curl);
+}catch(Exception $exc){
+	file_put_contents('error_log.txt', $exc->getMessage());
+}
