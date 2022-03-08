aa6db884e67a06444c351956214a7aec6d993688
Anna <17416592+zgal88@users.noreply.github.com>
2022-03-08 03:03:48 -0500

    Update index.php
    Added verification_token check to ensure the data payload isn't forged.

--- index.php
+++ index.php
@@ -9,9 +9,14 @@
 //  Listed under the MIT License                                                                       //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 try{
+	// IMPORTANT : Verification Token is used to ensure messages sent to your API are actually from Ko-Fi
+	// You can get your verification token on the same page you enabled your webhook on, under 'Advanced'
+	// Link: https://ko-fi.com/manage/webhooks
+	// If You Do Not Change This To Your Token This Plugin Will Not Work!
+	$verification_token = "12345678-abcd-1234-abcd-123456789123";
 	// send_discord_webhook : set to 'true' if you want to enable the Discord webhook sender.
 	// Assumes send-discord.php is in the same folder as this file.
-	//
+	// 
 	// hook_url : Required if sending - change this to your own webhook address. Will fail
 	// if you do not change it.
 	$send_discord_webhook = true;
@@ -22,40 +27,51 @@ try{
 			// Grabs The payload, and decodes it.
 			$data = $_POST['data'];
 			$array = json_decode($data, true);
-			// Assign variables from data array values.
-			$messageID = (($array['message_id'] != "") ? $array['message_id'] : "None");
-			$kofiTransactionID = (($array['kofi_transaction_id'] != "") ? $array['kofi_transaction_id'] : "None");
-			// When the donation was sent
-			$donationTimeStamp = (($array['timestamp'] != "") ? $array['timestamp'] : "None");
-			// The donation type - Possible Types Include Donation, Subscription, Commission, and Shop Order.
-			$donationType = (($array['type'] != "") ? $array['type'] : "None");
-			// Display name for who ssent the donation; not necessarily their Ko-Fi username.
-			$donationFrom = (($array['from_name'] != "") ? $array['from_name'] : "None");
-			// The email address of the sender
-			$donationEmail = (($array['email'] != "") ? $array['email'] : "None");
-			// If they left a message, it's here.
-			$donationMessage = (($array['message'] != "") ? $array['message'] : "None");
-			// The numerical value of the donation amount
-			$donationAmount = (($array['amount'] != "") ? $array['amount'] : "None");
-			// The donation currency
-			$donationCurrency = (($array['currency'] != "") ? $array['currency'] : "None");
-			// URL of donaiton
-			$donationURL = (($array['url'] != "") ? $array['url'] : "None");
-			// Is the donation a subscription?
-			$donationIsSubscription = (($array['is_subscription_payment'] != "") ? $array['is_subscription_payment'] : "None");
-			// Is iot the first payment in the subscription?
-			$donationIsFirstSubPayment = (($array['is_first_subscription_payment'] != "") ? $array['is_first_subscription_payment'] : "None");
-			
-			////////////////////////////////////////////////////////////////////////////
-			//                                                                        //
-			//                  Do Stuff Here With The Gathered Data                  //
-			//                                                                        //
-			////////////////////////////////////////////////////////////////////////////
-			
-			// Send Discord Webhook, If Enabled
-			if($send_discord_webhook == true) require('send-discord.php');
-			// Send the 200 Ok Header and end function
-			header($_SERVER["SERVER_PROTOCOL"]." 200 Ok");
+			// First Check If Verification Token Matches
+			if($array['verification_token'] == $verification_token){
+				// Verification Matched
+				// We can proceed with collecting and processing data 
+				// This is the internal Ko-Fi message ID
+				$messageID = (($array['message_id'] != "") ? $array['message_id'] : "None");
+				// Internal Ko-Fi transaction ID
+				$kofiTransactionID = (($array['kofi_transaction_id'] != "") ? $array['kofi_transaction_id'] : "None");
+				// When the donation was sent
+				$donationTimeStamp = (($array['timestamp'] != "") ? $array['timestamp'] : "None");
+				// The donation type - Possible Types Include Donation, Subscription, Commission, and Shop Order.
+				$donationType = (($array['type'] != "") ? $array['type'] : "None");
+				// Display name for who ssent the donation; not necessarily their Ko-Fi username.
+				$donationFrom = (($array['from_name'] != "") ? $array['from_name'] : "None");
+				// The email address of the sender
+				$donationEmail = (($array['email'] != "") ? $array['email'] : "None");
+				// If they left a message, it's here.
+				$donationMessage = (($array['message'] != "") ? $array['message'] : "None");
+				// The numerical value of the donation amount
+				$donationAmount = (($array['amount'] != "") ? $array['amount'] : "None");
+				// The donation currency
+				$donationCurrency = (($array['currency'] != "") ? $array['currency'] : "None");
+				// URL of donaiton
+				$donationURL = (($array['url'] != "") ? $array['url'] : "None");
+				// Is the donation a subscription?
+				$donationIsSubscription = (($array['is_subscription_payment'] != "") ? $array['is_subscription_payment'] : "None");
+				// Is iot the first payment in the subscription?
+				$donationIsFirstSubPayment = (($array['is_first_subscription_payment'] != "") ? $array['is_first_subscription_payment'] : "None");
+				
+				////////////////////////////////////////////////////////////////////////////
+				//                                                                        //
+				//                  Do Stuff Here With The Gathered Data                  //
+				//                                                                        //
+				////////////////////////////////////////////////////////////////////////////
+				
+				// Send Discord Webhook, If Enabled
+				if($send_discord_webhook == true) require('send-discord.php');
+				// Send the 200 Ok Header and end function
+				header($_SERVER["SERVER_PROTOCOL"]." 200 Ok");
+			}
+			if($array['verification_token'] != $verification_token){
+				// Do Something Here If You Want To Log The Mismatched Verifications
+				// This might be good information to log, if you want to keep track
+				// of possible fraudulent attempts on your site.
+			}
 		}catch(Exception $exc){
 			// Some shizz happened, so let's log it for you
 			$message = "\r\nException: " . $exc->getMessage();
